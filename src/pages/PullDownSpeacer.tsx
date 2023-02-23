760c5f1a3f5474572533fb42c379a59aede4e94d
Lnida <91710828+d4n1938@users.noreply.github.com>
2023-02-23 22:14:56 +0900

    delete unesed comments

--- src/pages/PullDownSpeacer.tsx
+++ src/pages/PullDownSpeacer.tsx
@@ -1,38 +1,53 @@
-import { type } from "os"
-import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal, ChangeEvent } from "react"
+import {
+  ReactElement,
+  JSXElementConstructor,
+  ReactFragment,
+  ReactPortal,
+  ChangeEvent,
+} from "react";
 
 const PullDownSpeacer = (props: { changeChara: (arg0: any) => void }) => {
-  const speacers = require("../../speakers.json")
+  const speacers = require("../../speakers.json");
 
-  type Speaker = { name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined }
+  type Speaker = {
+    name:
+      | string
+      | number
+      | boolean
+      | ReactElement<any, string | JSXElementConstructor<any>>
+      | ReactFragment
+      | ReactPortal
+      | null
+      | undefined;
+  };
 
   const pulldown = () => {
-    let list: JSX.Element[] = []
-      speacers.map((speaker:Speaker , index:number)=>{
-        list.push(<option value={index} key={index}>{speaker.name}</option>)
-      })
-    return list
-  }
+    let list: JSX.Element[] = [];
+    speacers.map((speaker: Speaker, index: number) => {
+      list.push(
+        <option value={index} key={index}>
+          {speaker.name}
+        </option>
+      );
+    });
+    return list;
+  };
 
-  const changeChr = (e: ChangeEvent<HTMLSelectElement>)=> {
-    console.log("e.target.value")
-    console.log(e.target.value)
-    props.changeChara(speacers[e.target.value]["styles"][0]["id"])
-    
-  }
+  const changeChr = (e: ChangeEvent<HTMLSelectElement>) => {
+    console.log("e.target.value");
+    console.log(e.target.value);
+    props.changeChara(speacers[e.target.value]["styles"][0]["id"]);
+  };
 
-   return (
-     <>
-     <select name="speacer" id="speaker" onChange={(e)=>changeChr(e)}>
+  return (
+    <>
+      <select name="speacer" id="speaker" onChange={(e) => changeChr(e)}>
         {pulldown()}
-     </select>
+      </select>
 
-{/* style--------------------------------------------------- */}
-<style jsx>
- {`
-`}
-</style>
-     </>
-   );
-}
+      {/* style--------------------------------------------------- */}
+      <style jsx>{``}</style>
+    </>
+  );
+};
 export default PullDownSpeacer;
